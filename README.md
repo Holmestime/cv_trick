2e19563c5a3314ecafcd564e13863318a0c99c2d
holmestime <1308072954@qq.com>
2020-04-11 08:56:01 +0800

    test

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## Introduction
 
 I will record some useful scripts and tricks during my learning of cv.
+
+
+
+test a
---
+++ gitpush.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# 打印操作
+function printStep(){
+  echo "### 执行操作【$1】###"
+}
+
+#判断 字符串参数1是否包含字符串参数2
+function countainStr(){
+  result=$(echo $1 | grep "${2}")
+  if [[ "$result" != "" ]]
+  then
+    return 1
+  else
+    return 0
+  fi
+}
+#ADD
+echo -e "\n"
+printStep "git add"
+echo `git add .`
+
+printStep "git status"
+echo -e "\n"
+statusResult=`git status`
+echo $statusResult
+
+# 如果没有文件修改
+countainStr $statusResult "nothing to commit"
+if [ $? == 1 ]
+then
+  echo "nothing done"
+  exit
+fi
+
+# 提交内容为空
+message="$1"
+if [ "$message" = "" ]
+then
+  echo "please input your explanation"
+  read $message
+fi
+
+printStep "git commit -m ${message}"
+echo `git commit -m ${message}`
+
+printStep "git push"
+pushResult=`git push`
+
+# 如果推送远程报错
+countainStr $pushResult "fatal: "
+if [ $? == 1 ]
+then
+  echo "something error!"
+else
+  echo "push successfully"
+fi
+
